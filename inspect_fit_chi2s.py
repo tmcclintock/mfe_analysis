be7a3b36ff5b6c161e71c67de53b72f32cb29eb1
Tom McClintock <tmcclintock@email.arizona.edu>
2017-02-06 09:44:00 -0700

    Have the full_chi2_models.txt data now to work with.

--- inspect_fit_chi2s.py
+++ inspect_fit_chi2s.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import chi2
 
-all_chi2s = np.genfromtxt("test_data/chi2_models.txt")
+all_chi2s = np.genfromtxt("test_data/full_chi2_models.txt")
 print all_chi2s.shape
 chi2s = all_chi2s.flatten()
 print chi2s.shape
 
-plt.hist(chi2s,20,normed=True) #Make the histogram
+plt.hist(chi2s,30,normed=True) #Make the histogram
 df = 10 #ish, sometimes 9
 mean,var,skew,kurt = chi2.stats(df,moments='mvsk')
 x = np.linspace(chi2.ppf(0.01,df),chi2.ppf(0.99,df),100)
